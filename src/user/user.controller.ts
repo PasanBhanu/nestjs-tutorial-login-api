387723dd7873aef5793c6955bd6b44114534057b
Pasan Bhanu Guruge <pasan@azbow.com>
2025-06-04 10:32:43 +0530

    Add auth guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { UserDetailsResponse } from './dto/response/user.response.dto';
+import { FirebaseTokenGuard } from '../common/guards/firebasetoken.guard';
 
 @Controller('user')
 export class UserController {
+  @UseGuards(FirebaseTokenGuard)
   @Get('get')
-  @ApiOkResponse({ type: UserDetailsResponse, description: 'success' })
+  @ApiOkResponse({
+    type: UserDetailsResponse,
+    description: 'success',
+  })
   appUserInformation(): UserDetailsResponse {
     const response = new UserDetailsResponse();
     return response;
